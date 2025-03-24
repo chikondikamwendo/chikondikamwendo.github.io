9fba7a1a39cd6968ca540096671ff595b94656c9
Chikondi Kamwendo <kondie208@gmail.com>
2025-03-24 10:16:43 +0200

    Refactor: Rearragned layout.

--- src/layouts/guest.tsx
+++ src/layouts/guest.tsx
@@ -36,9 +36,9 @@ const socialLinks = [
 
 export default function Guest() {
     return (
-        <main className="max-w-7xl mx-auto h-screen flex flex-col relative overflow-hidden">
-            <header className="py-5">
-                <nav className="w-full">
+        <>
+            <header className="py-5 fixed top-0 left-0 w-full z-50">
+                <nav className="max-w-7xl mx-auto">
                     <ul className="flex justify-start items-center gap-8">
                         <li>
                             <Link to="/">
@@ -67,27 +67,33 @@ export default function Guest() {
                 </nav>
             </header>
 
-            <section className="w-full flex-1">
+            <section className="max-w-7xl mx-auto h-screen relative">
                 <Outlet />
             </section>
 
-            <footer className="py-5 w-full">
-                <ul className="flex items-center gap-4">
-                    {socialLinks.map(({ icon: Icon, path }) => (
-                        <li key={path}>
-                            <a href={path} target="_blank" rel="noreferrer">
-                                <Icon className="text-2xl text-secondary-foreground/80 hover:text-secondary-foreground transition-colors" />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+            <footer className="py-5 w-full fixed z-50 bottom-0 left-0">
+                <section className="max-w-7xl mx-auto">
+                    <ul className="flex items-center gap-4">
+                        {socialLinks.map(({ icon: Icon, path }) => (
+                            <li key={path}>
+                                <a href={path} target="_blank" rel="noreferrer">
+                                    <Icon className="text-2xl text-secondary-foreground/80 hover:text-secondary-foreground transition-colors" />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
             </footer>
 
-            <div className="absolute -right-[32%] -translate-x-[1%] bottom-1/2 translate-y-1/2 -z-10  -rotate-90">
-                <p className="text-[10rem] text-stroke opacity-15">
-                    Hello World
-                </p>
-            </div>
-        </main>
+            <section className="w-full h-screen top-0 left-0 absolute -z-10">
+                <div className="max-w-7xl mx-auto h-full relative">
+                    <div className="absolute -right-[32%] -translate-x-[1%] bottom-1/2 translate-y-1/2  -rotate-90">
+                        <p className="text-[10rem] text-stroke opacity-15">
+                            Hello World
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
