ec7385ea5bff58fe2f765eca620c24af760271ec
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-08-29 20:33:59 +0200

    Feat: Added navigation.

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,4 +1,9 @@
+import Github from "@/components/icons/github";
+import Linkedin from "@/components/icons/linkedin";
+import Moon from "@/components/icons/moon";
+import Twitter from "@/components/icons/twitter";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function DefaultLayout({
   children,
@@ -7,13 +12,77 @@ export default function DefaultLayout({
 }) {
   return (
     <main className="container mx-auto border-l min-h-screen">
-        <header>
-            <ul>
-                <li>
-                    <Button></Button>
-                </li>
-            </ul>
-        </header>
+      <header className="px-4 py-6 flex justify-between items-center">
+        <ul className="flex items-center gap-2">
+          <li>
+            <Button
+              variant="ghost"
+              className="w-12 h-12 text-secondary-foreground/80 hover:text-primary hover:bg-primary/10"
+              asChild
+            >
+              <a href="https://github.com/chikondikamwendo" target="__blank">
+                <Github className="size-7" />
+              </a>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="ghost"
+              className="w-12 h-12 text-secondary-foreground/80 hover:text-primary hover:bg-primary/10"
+              asChild
+            >
+              <a
+                href="https://linkedin.com/in/chikondikamwendo"
+                target="__blank"
+              >
+                <Linkedin className="size-7" />
+              </a>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="ghost"
+              className="w-12 h-12 text-secondary-foreground/80 hover:text-primary hover:bg-primary/10"
+              asChild
+            >
+              <a href="https://x.com/chikondkamwendo" target="__blank">
+                <Twitter className="size-7" />
+              </a>
+            </Button>
+          </li>
+        </ul>
+
+        <ul className="flex items-center gap-2">
+          <li>
+            <Button
+              variant="ghost"
+              size="lg"
+              className="text-lg text-secondary-foreground/80 hover:text-primary hover:bg-primary/10"
+              asChild
+            >
+              <a href="#projects">Projects</a>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="ghost"
+              size="lg"
+              className="text-lg text-secondary-foreground/80 hover:text-primary hover:bg-primary/10"
+              asChild
+            >
+              <a href="#contact-me">Contact Me</a>
+            </Button>
+          </li>
+          <li className="h-8 px-4">
+            <Separator orientation="vertical" />
+          </li>
+          <li>
+            <Button variant="ghost" className="w-12 h-12 text-secondary-foreground/80 hover:text-primary hover:bg-primary/10">
+              <Moon className="size-7" />
+            </Button>
+          </li>
+        </ul>
+      </header>
       {children}
     </main>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import DefaultLayout from "@/layouts/default";
 export default function Home() {
   return (
     <DefaultLayout>
-      <h1>Home</h1>
     </DefaultLayout>
   )
 }
