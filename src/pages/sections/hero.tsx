4feacfae02a4c05e7b05b1249e7894d090ae8dc9
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-08-29 21:27:38 +0200

    Feat: Optimized hero section for responsiveness.

--- src/pages/sections/hero.tsx
+++ src/pages/sections/hero.tsx
@@ -7,40 +7,48 @@ import React from "@/components/icons/react";
 import Tailwind from "@/components/icons/tailwind";
 import TypeScript from "@/components/icons/typescript";
 import Vue from "@/components/icons/vue";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-const languagesAndFrameworks: { icon: React.ReactNode, tooltip: string }[] = [
-    { icon: <PHP />, tooltip: "PHP" },
-    { icon: <Python />, tooltip: "Python" },
-    { icon: <TypeScript />, tooltip: "TypeScript" },
-    { icon: <Dart />, tooltip: "Dart" },
-    { icon: <Laravel />, tooltip: "Laravel" },
-    { icon: <Flutter />, tooltip: "Flutter" },
-    { icon: <React />, tooltip: "React.js" },
-    { icon: <Vue />, tooltip: "Vue.js" },
-    { icon: <Tailwind />, tooltip: "Tailwind CSS" },
-]
+const languagesAndFrameworks: { icon: React.ReactNode; tooltip: string }[] = [
+  { icon: <PHP />, tooltip: "PHP" },
+  { icon: <Python />, tooltip: "Python" },
+  { icon: <TypeScript />, tooltip: "TypeScript" },
+  { icon: <Dart />, tooltip: "Dart" },
+  { icon: <Laravel />, tooltip: "Laravel" },
+  { icon: <Flutter />, tooltip: "Flutter" },
+  { icon: <React />, tooltip: "React.js" },
+  { icon: <Vue />, tooltip: "Vue.js" },
+  { icon: <Tailwind />, tooltip: "Tailwind CSS" },
+];
 
 export default function Hero() {
   return (
-    <section className="w-full h-[calc(100vh-10rem)] flex flex-col justify-end items-start">
-      <div className="p-8">
-        <h1 className="text-4xl font-semibold">Chikondi Kamwendo</h1>
-        <p className="text-3xl mt-2 text-muted-foreground">
+    <section className="w-full h-[calc(100vh-10rem)] flex flex-col justify-end items-center sm:items-start text-center sm:text-left">
+      <div className="p-4 sm:p-8">
+        <h1 className="text-2xl sm:text-4xl font-semibold">
+          Chikondi Kamwendo
+        </h1>
+        <p className="text-lg sm:text-3xl mt-2 text-muted-foreground">
           27 years old - he/him | Malawian
         </p>
       </div>
-      <div className="mt-4 border-l-8 border-primary px-4">
-        <h1 className="text-8xl font-semibold text-primary">
+      <div className="mt-4 border-l-0 sm:border-l-8 border-primary px-2 sm:px-4">
+        <h1 className="text-6xl sm:text-8xl font-semibold text-primary">
           Full Stack
-          <p className="text-9xl">Developer</p>
+          <p className="text-7xl sm:text-9xl">Developer</p>
         </h1>
       </div>
-      <div className="mt-10 flex items-center gap-6 px-8">
+      <div className="mt-10 flex justify-center sm:justify-start items-center gap-3 sm:gap-6 px-2 sm:px-8 flex-wrap">
         {languagesAndFrameworks.map((language, index) => (
           <Tooltip key={index}>
             <TooltipTrigger>
-              <p className="text-4xl text-secondary-foreground/75">{language.icon}</p>
+              <p className="text-3xl sm:text-4xl text-secondary-foreground/75">
+                {language.icon}
+              </p>
             </TooltipTrigger>
             <TooltipContent>
               <p>{language.tooltip}</p>
